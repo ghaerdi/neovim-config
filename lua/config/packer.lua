6c9ba108d4de9ee28efe49d5bb4dc8318e4436e9
ghaerdi <ghaerdi@outlook.com>
2023-03-27 09:02:59 -0400

    feat: add copilot

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -33,6 +33,7 @@ return require("packer").startup(function(use)
 	use("hrsh7th/nvim-cmp") -- completion plugin
 	use("hrsh7th/cmp-buffer") -- source for text in buffer
 	use("hrsh7th/cmp-path") -- source for file system paths
+	use("github/copilot.vim") -- github copilot
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") -- snippet engine
