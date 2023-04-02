3ee32988a94a642e11fab1a7e9bfe0fc12a594f3
ghaerdi <ghaerdi@outlook.com>
2023-04-01 23:02:49 -0400

    autocomplete copilot with <C-a>

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -15,9 +15,6 @@ if not lspkind_status then
 	return
 end
 
--- load vs-code like snippets from plugins (e.g. friendly-snippets)
-require("luasnip/loaders/from_vscode").lazy_load()
-
 vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,1 +1,2 @@
+vim.cmd[[imap <silent><script><expr> <C-a> copilot#Accept('\<CR>')]]
 vim.g.copilot_no_tab_map = true
