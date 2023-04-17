d685886e3f6da20f0ceea50aa47928aea03431e3
ghaerdi <ghaerdi@outlook.com>
2023-04-16 23:13:13 -0400

    feat: bind wsl neovim with windows clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,6 +19,18 @@ opt.softtabstop = 2
 
 -- clipboard
 opt.clipboard:append("unnamedplus")
+vim.g.clipboard = {
+	name = "WslClipboard",
+	copy = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe",
+	},
+	paste = {
+		["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+		["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+	},
+	cache_enabled = 0,
+}
 
 -- line wrapping
 opt.wrap = false
