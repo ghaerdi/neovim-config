b4a67f31ad0ef0dd248885052c0cd7bf57bd9854
ghaerdi <ghaerdi@outlook.com>
2023-04-17 19:59:00 -0400

    feat: don't paste from system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,10 +25,10 @@ vim.g.clipboard = {
 		["+"] = "clip.exe",
 		["*"] = "clip.exe",
 	},
-	paste = {
-		["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
-		["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
-	},
+	-- paste = {
+	-- 	["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+	-- 	["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+	-- },
 	cache_enabled = 0,
 }
 
